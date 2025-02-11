a0452ceaa23d3123e927646f8a67f2fe69203887
Jinno Tomoyuki <tomoyuki-j@elm12>
2025-02-11 17:49:18 +0900

    Fixed a bug

--- set_data.sh
+++ set_data.sh
@@ -4,9 +4,9 @@ cp -r SimKGC-main/data/WN18RR/ data
 
 relation_type=has_part
 
-cp custom_data/${relation_type}_train.tsv data/WN18RR/train.txt
-cp custom_data/${relation_type}_test.tsv data/WN18RR/test.txt 
-cp custom_data/${relation_type}_valid.tsv data/WN18RR/valid.txt
+cp custom_data/${relation_type}_train.txt data/WN18RR/train.txt
+cp custom_data/${relation_type}_test.txt data/WN18RR/test.txt 
+cp custom_data/${relation_type}_valid.txt data/WN18RR/valid.txt
 
 bash scripts/preprocess.sh WN18RR
 
